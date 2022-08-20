1aa2232f6aa27a637f043d88bb9e9d1b574c7519
SaraiNoQ <2994498974@qq.com>
2022-08-20 23:39:38 +0800

    fix: style issue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
   <div>
-    <!-- <Navbar /> -->
     <div id="app">
       <RouterView />
     </div>
-    <!-- <Footer /> -->
   </div>
 </template>
 <script setup lang="ts">
 import { logLabeled } from "./utils/logger";
-// import Navbar from "./components/layout/Navbar/index.vue";
-// import Footer from "./components/layout/Footer/index.vue";
 
 logLabeled("I'm ready!  ⸜(๑'ᵕ'๑)⸝⋆*", "log", "", "color: #66ccff");
 </script>
