4bfd02c46414da7a1a84534a9af829935ee3e14b
SaraiNoQ <2994498974@qq.com>
2022-08-21 22:35:09 +0800

    fix: enviroment file

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const instance = axios.create();
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_APP_URL,
+});
 
 // 设置请求拦截器
 instance.interceptors.request.use(
